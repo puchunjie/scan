ce9b87be1916867b65fffe05a2a46d6641510c97
jeremy.PCJ <puchunjie@live.com>
2023-02-22 20:32:09 +0800

    feat： 111

--- api/index.js
+++ api/index.js
@@ -12,6 +12,21 @@ const selectSQL = (sqll, success, fail) => {
 	});
 }
 
+// 执行sql
+const executeSQL = (sqll, success, fail) => {
+	plus.sqlite.executeSql({
+		name: 'first',
+		sql: sqll,
+		success(e) {
+			success?.(e)
+		},
+		fail(e) {
+			fail(e)
+		}
+	});
+
+}
+
 // 初始化数据库，在APP install的时候执行
 export const createDb = () => {}
 
@@ -22,7 +37,20 @@ export const openDb = () => {}
 export const scanOne = () => {}
 
 // 创建、编辑订单
-export const createOrder = () => {}
+export const createOrder = ({
+	batchNo,
+	productDate,
+	factoryCode,
+	line,
+	sku
+}, success, fail) => {
+	let sqll =
+		'insert into t_order values(null,\':batchNo\',\':productDate\',\':factoryCode\',\':line\',\':sku\',0,CURRENT_TIMESTAMP)';
+	sqll = sqll.replace(':batchNo', batchNo).replace(':productDate', productDate).replace(':factoryCode',
+			factoryCode)
+		.replace(':line', line).replace(':sku', sku)
+	executeSQL(sqll, success, fail)
+}
 
 // 订单查询
 export const queryOrderList = () => {}
